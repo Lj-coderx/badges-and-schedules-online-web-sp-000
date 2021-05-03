59a65c4790cc5b32359d7f24ba7b1d392a868a98
Lj <tnedutsl457892@gmail.com>
2021-05-03 09:24:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(name)
+  new_array = []
+ counter = 1
+ arr.each do |name|
+ new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+ counter+=1
+ end
+ return new_array  
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(array).each do |assignment|
+    puts assignment
+  end
